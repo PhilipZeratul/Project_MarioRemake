32094eca9a5d2be0e53b5d7e51a274fdaaee7580
Phillip Zhang <phillipzhang@Phillips-MacBook-Pro.local>
2018-11-07 00:49:03 +0800

    Added Jump and Running Animation

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -11,6 +11,13 @@ public class PlayerController : MonoBehaviour
     private PhysicsObject physicsObject;
     private ControllerManager controllerManager;
     private float jumpStartTime;
+    private bool isFacingRight = true;
+
+    private Animator animator;
+    private readonly int jumpHash = Animator.StringToHash("Jump");
+    private readonly int isGroundedHash = Animator.StringToHash("IsGrounded");
+    private readonly int isRunningHash = Animator.StringToHash("IsRunning");
+
 
     [Inject]
     private void Init(ControllerManager _controllerManager)
@@ -21,16 +28,25 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         physicsObject = GetComponent<PhysicsObject>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.SetBool(isGroundedHash, physicsObject.IsGrounded());
+        animator.SetBool(isRunningHash, !MyUtility.NearlyEqual(physicsObject.GetVelocityX(), 0f));
     }
 
     private void FixedUpdate()
     {
         float velocityX = controllerManager.HorizontalMove() * horizontalSpeed;
 
+        if ((velocityX > 0 && !isFacingRight) || (velocityX < 0 && isFacingRight))
+            Flip();
+
         if (physicsObject.IsGrounded() && controllerManager.JumpPressed())
-        {
-            physicsObject.Move(velocityX, jumpSpeed);
-            jumpStartTime = Time.time;
+        {        
+            Jump(velocityX);
         }
         else if (physicsObject.GetVelocityY() > 0f && controllerManager.JumpHolding())
         {
@@ -42,5 +58,19 @@ public class PlayerController : MonoBehaviour
             physicsObject.Move(velocityX);
 
         controllerManager.JumpPressedConsumed();
+    }
+
+    private void Jump(float velocityX)
+    {
+        physicsObject.Move(velocityX, jumpSpeed);
+        jumpStartTime = Time.time;
+
+        animator.SetTrigger(jumpHash);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 }
