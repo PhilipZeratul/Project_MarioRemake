30162f3636a0940238bd25e399ebf42fab6154bd
Phillip Zhang <phillipzhang@Phillips-MacBook-Pro.local>
2018-11-07 22:56:04 +0800

    Added Some Arts

---
+++ Assets/_Scripts/Particle/SelfDestroyTimer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+public class SelfDestroyTimer : MonoBehaviour
+{
+    public float surviveTime = 2f;
+    private float startTime;
+
+
+    private void Start()
+    {
+        startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time - startTime > surviveTime)
+            Destroy(gameObject);
+    }
+}
