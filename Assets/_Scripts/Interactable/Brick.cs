14bbe4f1319223a8e3a38bd2db643cb7641e0631
Phillip Zhang <phillipzhang@Phillips-MacBook-Pro.local>
2018-11-07 22:39:15 +0800

    Added Brick Break Particle Animation

--- Assets/_Scripts/Interactable/Brick.cs
+++ Assets/_Scripts/Interactable/Brick.cs
@@ -3,9 +3,16 @@
 
 public class Brick : MonoBehaviour, IInteractableObject
 {
+    public GameObject brickBreakParticleGO;
+
+
     public void IsHit(GameObject source, Constants.HitDirection dir)
     {
         if (dir == Constants.HitDirection.Bottom)
+        {
+
+            Instantiate(brickBreakParticleGO, transform.position, transform.rotation);
             Destroy(gameObject);
+        }
     }
 }
