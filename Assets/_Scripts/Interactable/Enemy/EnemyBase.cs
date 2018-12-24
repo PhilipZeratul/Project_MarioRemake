731f9518d8d3be3cb04c31460e9b8761e832b27b
Phillip Zhang <phillipzhang@Phillips-MacBook-Pro.local>
2018-12-24 22:38:24 +0800

    Doing Enemy Move

--- Assets/_Scripts/Interactable/Enemy/EnemyBase.cs
+++ Assets/_Scripts/Interactable/Enemy/EnemyBase.cs
@@ -3,25 +3,61 @@
 
 public class EnemyBase : MonoBehaviour, IInteractableObject
 {
+    public Transform[] wayPoints;
+    public float moveSpeed = 2f;
+
+    private int currentWayPoint;
+    private PhysicsObject physicsObject;
+
+
+    private void Start()
+    {
+        if (wayPoints.Length > 0)
+            physicsObject = GetComponent<PhysicsObject>();
+    }
+
+    private void FixedUpdate()
+    {
+        float fixedDeltaTime = Time.fixedDeltaTime;
+
+        if (wayPoints.Length > 0)
+        {
+            Vector3 direction = (wayPoints[currentWayPoint].position - transform.position).normalized;
+            physicsObject.Move(direction.x * moveSpeed, direction.y * moveSpeed);
+
+            float distance = Vector3.Distance(transform.position, wayPoints[currentWayPoint].position);
+            if (distance < moveSpeed * fixedDeltaTime)
+                currentWayPoint = (currentWayPoint + 1) % wayPoints.Length;
+
+            Debug.LogFormat("currentWayPoint = {0}, direction = {3}, distance = {1}, threshold = {2}", currentWayPoint, distance, moveSpeed * fixedDeltaTime, direction);
+        }
+    }
+
     public void IsHit(GameObject source, Constants.HitDirection from)
     {
         PlayerController player = source.GetComponent<PlayerController>();
 
-        if (player == null)
-            return;        
-
-        if (from == Constants.HitDirection.Top)
+        if (player != null)
         {
-            player.BounceUponEnemy();
-            Die();
+            if (from == Constants.HitDirection.Top)
+            {
+                player.BounceUponEnemy();
+                Die();
+            }
+            else
+            {
+                // Kill Player
+                Debug.LogFormat("Enemy: {0} Kill Player", gameObject.name);
+                player.HitByEnemy();
+            }
         }
         else
         {
-            // Kill Player
-            Debug.LogFormat("Enemy: {0} Kill Player", gameObject.name);
-            player.HitByEnemy();
+            //~TODO: Return when hit wall.
+            //currentWayPoint--;
+            //if (currentWayPoint < 0)
+                //currentWayPoint = wayPoints.Length - 1;
         }
-
     }
 
     protected void Die()
